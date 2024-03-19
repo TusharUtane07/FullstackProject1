efcc1427a3e21b87c53ef94726e4afb4f8d2649a
Tushar <tusharutane2@gmail.com>
2024-03-19 13:51:24 +0530

    Added login and logout functionality in router & middleware for checking authenticated user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+	loginUser,
+	logoutUser,
+	registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/Multer.middleware.js";
+import { verifyJWT } from "../middlewares/Auth.middleware.js";
 
 const router = Router();
 
+// added what to do on this particular route
 router.route("/register").post(
 	upload.fields([
 		{
@@ -17,5 +23,12 @@ router.route("/register").post(
 	]),
 	registerUser
 );
+
+// added what to do on this particular route
+router.route("/login").post(loginUser);
+
+// Secured Routes
+// added middleware to check verified user then do the functionality provided on this particular route
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
