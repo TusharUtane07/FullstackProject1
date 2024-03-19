f6cfb1a1f5679dfe847312a4878b62b30e8ef36a
Tushar <tusharutane2@gmail.com>
2024-03-19 20:40:55 +0530

    Added Subscription Model

---
+++ src/models/subscriptions.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+	{
+		subscription: {
+			type: Schema.Types.ObjectId,
+			ref: "User", // The one who is subscribing
+		},
+		channel: {
+			type: Schema.Types.ObjectId,
+			ref: "User", // The one who got subscribed
+		},
+	},
+	{ timestamps: true }
+);
+
+export const Subscriptions = mongoose.model(
+	"Subscriptions",
+	subscriptionSchema
+);
